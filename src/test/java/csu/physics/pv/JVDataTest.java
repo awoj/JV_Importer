bcf7c7254a709013c30bff946d0a8ac51db64fc9
awoj <anna.woj@gmail.com>
2017-09-19 11:53:53 -0600

    fixed build issues, added dummy test

---
+++ src/test/java/csu/physics/pv/JVDataTest.java
@@ -0,0 +1,19 @@
+package csu.physics.pv;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+
+public class JVDataTest extends TestCase {
+
+    JVData data;
+
+    @Test
+    public void testSetFileNames() {
+
+        int i = 0;
+        assertTrue(i == 0);
+
+    }
+
+}
